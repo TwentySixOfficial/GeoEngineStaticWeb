646d9724a69265c3cbae3d23ed36f7a69299b3a3
TwentySiX <140285549+TwentySixOfficial@users.noreply.github.com>
2025-09-10 03:01:41 +0000

    updated css file

--- src/scripts/js/index.js
+++ src/scripts/js/index.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const url = window.location.href.split("/");
-    console.log(url);
     const msg = document.getElementById('message');
     msg.textContent = "Frontend is working.";
-})
+});
