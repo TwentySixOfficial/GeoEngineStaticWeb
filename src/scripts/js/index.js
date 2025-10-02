846cc625045bb7062b0d735de386c9674786ebf2
TwentySiX <140285549+TwentySixOfficial@users.noreply.github.com>
2025-10-02 22:09:59 +0000

    added thing

--- src/scripts/js/index.js
+++ src/scripts/js/index.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const msg = document.getElementById('message');
     msg.textContent = "Frontend is working.";
+    const msg2 = document.getElementById('message2');
+    msg2.textContent = "Hi caiden so basically this is my website but idk about\nadding feet pics lol.";
 });
